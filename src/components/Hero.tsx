611b06bfa002ec3b04a6267544aa35e8a9e18fbd
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-04-08 10:03:02 -0400

    added Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Image from "next/image";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
+
+type Props = {};
+
+function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: ["Hi, I'm Nick!", "Software Engineer", "Passionate about Software"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      {/* TODO: Get an _actual_ picture of myself to put here */}
+      <Image
+        alt="Picture of Me"
+        src={
+          "https://w7.pngwing.com/pngs/115/800/png-transparent-drum-set-illustration-rock-band-4-the-beatles-rock-band-computer-icons-musical-ensemble-drum-stick-drum-area-music-festival.png"
+        }
+        width={300}
+        height={300}
+        className="relative rounded-full h-48 w-48 mx-auto object-cover"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A"></Cursor>
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import Header from "@/components/Header";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
 
       {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
 
       {/* About */}
 
