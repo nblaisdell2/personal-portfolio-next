7dc8b3a4ca5a5483a31ac392eef38fc6a0dcc133
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-04-08 11:01:48 -0400

    added About section

---
+++ src/components/About.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function About({}: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        About
+      </h3>
+
+      <motion.img
+        className="rounded-full mt-16 -mb-16 md:mb-0 flex-shrink-0 w-48 h-48 object-cover md:w-64 md:h-64"
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        // viewport={{ once: true }}
+        src="https://w7.pngwing.com/pngs/115/800/png-transparent-drum-set-illustration-rock-band-4-the-beatles-rock-band-computer-icons-musical-ensemble-drum-stick-drum-area-music-festival.png"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#186bd7]/50">little</span>{" "}
+          background
+        </h4>
+        {/* TODO: Input a _real_ background about myself */}
+        <p className="text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          molestie nec nunc ut scelerisque. Etiam ac felis vitae lorem posuere
+          maximus. Aliquam interdum eget tortor sed scelerisque. Donec nulla
+          lorem, elementum eu erat interdum, condimentum faucibus felis. Integer
+          in ante nibh. Donec ut volutpat ex. Quisque ex quam, finibus at justo
+          eget, porttitor volutpat arcu. Donec pulvinar sapien non blandit
+          ullamcorper. Donec ipsum ante, pretium consectetur luctus at, congue
+          commodo nibh. Cras et volutpat nulla. Phasellus auctor leo nibh, nec
+          eleifend sapien imperdiet consectetur. Curabitur condimentum urna vel
+          facilisis rhoncus. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit.
+        </p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      {/* Header */}
       <Header />
 
       {/* Hero */}
@@ -12,6 +14,9 @@ export default function Home() {
       </section>
 
       {/* About */}
+      <section id="about" className="snap-start">
+        <About />
+      </section>
 
       {/* Experience */}
 
